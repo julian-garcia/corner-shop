8e100859937b078a14fce3ca7e3604874f4b3cb6
Julian Garcia <julian.garcia.leoni@gmail.com>
2019-01-15 14:07:16 +0000

    Navigation and shopping cart components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
